e008da458d8477ddaf2138a8a0e4ef8a8749fcc8
gurpreet <gurpreets808@gmail.com>
2024-06-13 14:29:13 -0300

    Estilo de app y navbar funcional

--- src/app/componentes/nav-bar/nav-bar.component.ts
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule,
+    ButtonModule
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
+  menu_items: MenuItem[] = [
+    { label: 'Home', icon: 'fa-solid fa-house', routerLink: '/' },
+    { label: 'Usuarios', icon: 'fa-solid fa-user', routerLink: '/usuarios' },
+    { label: 'Obras Sociales', icon: 'fa-solid fa-clinic-medical', routerLink: '/obras-sociales' },
+    { label: 'Especialidades', icon: 'fa-solid fa-stethoscope', routerLink: '/especialidades' },
+    { label: 'Turnos', icon: 'fa-solid fa-calendar-alt', routerLink: '/turnos' }
+  ];
+
+  constructor(public router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  Desloguear() {
+    //this.servAuth.LogOut();
+    this.router.navigateByUrl('/login');
+  }
 }
