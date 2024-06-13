af800a558dd0aba9bb9ec40124084028eff3e029
gurpreet808 <gurpreets808@gmail.com>
2024-06-13 19:56:02 -0300

    Base para registarse

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    ButtonModule
+  ],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss'
 })
 export class RegistroComponent {
+  tipo_usuario: string = '';
+
+  SeleccionarTipo(tipo_usuario: string) {
+    console.log(tipo_usuario);
+    this.tipo_usuario = tipo_usuario;
+  }
 
 }
