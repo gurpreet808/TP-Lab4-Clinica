0f395229985cb5097a97264a369361d6ed5809b2
gurpreet808 <gurpreets808@gmail.com>
2024-06-13 02:04:28 -0300

    Agrego componentes basicos

---
+++ src/app/pages/obras-sociales/obras-sociales.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obras-sociales',
+  standalone: true,
+  imports: [],
+  templateUrl: './obras-sociales.component.html',
+  styleUrl: './obras-sociales.component.scss'
+})
+export class ObrasSocialesComponent {
+
+}
