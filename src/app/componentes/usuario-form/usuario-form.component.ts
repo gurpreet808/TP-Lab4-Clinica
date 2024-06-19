2d879165334b9a4fe121dfc0e8b87b03219f2767
gurpreet <gurpreets808@gmail.com>
2024-06-19 15:45:37 -0300

    Correccion de estilos

--- src/app/componentes/usuario-form/usuario-form.component.ts
+++ src/app/componentes/usuario-form/usuario-form.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
 import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SelectItem, MessageService } from 'primeng/api';
-import { DisponibilidadEspecialidad, Disponibilidad } from '../../clases/disponibilidad';
 import { Especialidad } from '../../clases/especialidad';
 import { ObraSocial } from '../../clases/obra-social';
 import { Usuario, Especialista, Paciente } from '../../modulos/auth/clases/usuario';
@@ -17,13 +16,13 @@ import { ExisteStringValidator } from '../../validators/existeStringValidator';
 import { map } from 'rxjs';
 import { confirmarCalveValidator } from '../../validators/confirmarClaveValidator';
 import { requeridoSegunTipoUsuario } from '../../validators/requeridoSegunTipoUsuarioValidator';
-import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { PasswordModule } from 'primeng/password';
 import { MultiSelectModule } from 'primeng/multiselect';
+import { InputGroupModule } from 'primeng/inputgroup';
 import { ShowValidationErrorsDirective } from '../../directivas/show-validation-errors.directive';
 import { ValidatorMessage } from '../../clases/validator-message';
 import { DiaPipe } from '../../pipes/dia.pipe';
@@ -35,13 +34,13 @@ import { EspecialidadPipe } from '../../pipes/especialidad.pipe';
   imports: [
     FormsModule,
     ReactiveFormsModule,
-    CommonModule,
     InputTextModule,
     ButtonModule,
     DropdownModule,
     InputNumberModule,
     PasswordModule,
     MultiSelectModule,
+    InputGroupModule,
     ShowValidationErrorsDirective,
     DiaPipe,
     EspecialidadPipe
@@ -59,32 +58,13 @@ export class UsuarioFormComponent implements OnInit, OnChanges {
   file_1: File | undefined;
   file_2: File | undefined;
 
-  new_disponibilidad: DisponibilidadEspecialidad = {
-    dia: -1,
-    hora_fin: 0,
-    hora_inicio: 0,
-    especialidad: '',
-  }
-
-  min_hora_inicio: number = 0;
-  max_hora_fin: number = 23;
-
-  dias: SelectItem[] = [
-    { label: 'Lunes', value: 1 },
-    { label: 'Martes', value: 2 },
-    { label: 'Miércoles', value: 3 },
-    { label: 'Jueves', value: 4 },
-    { label: 'Viernes', value: 5 },
-    { label: 'Sábado', value: 6 },
-    { label: 'Domingo', value: 0 }
-  ];
-
   mensajes_validacion: ValidatorMessage = {
     required: 'Debe completar este campo.',
     min: 'Este campo debe ser como mínimo {requiredLength}.',
     max: 'Este campo debe ser como máximo {requiredLength}.',
     pattern: 'El formato de este campo no es válido.',
-    existeString: 'Ya existe ese valor.'
+    existeString: 'Ya existe ese valor.',
+    noCoincide: 'Las claves no coinciden.',
   };
 
   constructor(
@@ -126,7 +106,6 @@ export class UsuarioFormComponent implements OnInit, OnChanges {
         url_foto_2: ['',],
         obra_social: ['',],
         especialidades: ['',],
-        disponibilidades: ['',],
       }
     );
 
@@ -144,7 +123,6 @@ export class UsuarioFormComponent implements OnInit, OnChanges {
       this.userForm.get('obra_social')?.setValidators([requeridoSegunTipoUsuario(this.tipo_usuario, ['paciente'])]);
       this.userForm.get('url_foto_2')?.setValidators([requeridoSegunTipoUsuario(this.tipo_usuario, ['paciente'])]);
       this.userForm.get('especialidades')?.setValidators([requeridoSegunTipoUsuario(this.tipo_usuario, ['especialista'])]);
-      this.userForm.get('disponibilidades')?.setValidators([requeridoSegunTipoUsuario(this.tipo_usuario, ['especialista'])]);
       this.userForm.updateValueAndValidity();
     }
   }
@@ -161,7 +139,8 @@ export class UsuarioFormComponent implements OnInit, OnChanges {
     let control = this.getControl(control_name);
 
     if (control != null) {
-      return control.invalid;
+      return (control.invalid && control.touched);
+      //return control.invalid;
     }
 
     return false;
@@ -192,13 +171,14 @@ export class UsuarioFormComponent implements OnInit, OnChanges {
 
       switch (usuario.tipo) {
         case "especialista":
-          (usuario as Especialista).habilitado = false;
           if (this.servAuth.usuarioActual.value && this.servAuth.usuarioActual.value.tipo == 'admin') {
             (usuario as Especialista).habilitado = true;
+          } else {
+            (usuario as Especialista).habilitado = false;
           }
-          let especialidades: Especialidad[] = this.getControlValue('especialidades');
+
           (usuario as Especialista).especialidades = this.getControlValue('especialidades').map((esp: Especialidad) => esp.id);
-          (usuario as Especialista).disponibilidades = this.getControlValue('disponibilidades');
+          (usuario as Especialista).disponibilidades = [];
           break;
 
         case "paciente":
@@ -354,100 +334,5 @@ export class UsuarioFormComponent implements OnInit, OnChanges {
 
     console.log(this.file_1);
     console.log(this.file_2);
-  }
-
-  AgregarDisponibilidad() {
-    console.log(this.new_disponibilidad);
-
-    if (this.new_disponibilidad.hora_fin <= this.new_disponibilidad.hora_inicio) {
-      this.messageService.add({ severity: 'error', life: 10000, summary: 'Error', detail: 'La hora de fin debe ser mayor a la de inicio' });
-      return;
-    }
-
-    if (this.new_disponibilidad.dia == -1) {
-      this.messageService.add({ severity: 'error', life: 10000, summary: 'Error', detail: 'Debe elegir un día' });
-      return;
-    }
-
-    if (this.new_disponibilidad.especialidad == '') {
-      this.messageService.add({ severity: 'error', life: 10000, summary: 'Error', detail: 'Debe elegir una especialidad' });
-      return;
-    }
-
-    if (this.new_disponibilidad.hora_inicio < this.min_hora_inicio) {
-      this.messageService.add({ severity: 'error', life: 10000, summary: 'Error', detail: `La clínica está disponible desde las ${this.min_hora_inicio}:00 en ese día` });
-      return;
-    }
-
-    if (this.new_disponibilidad.hora_fin > this.max_hora_fin) {
-      this.messageService.add({ severity: 'error', life: 10000, summary: 'Error', detail: `La clínica está disponible hasta las ${this.max_hora_fin}:00 en ese día` });
-      return;
-    }
-
-    let disponibilidades: DisponibilidadEspecialidad[] = this.getControlValue('disponibilidades');
-    let overlap = false;
-    for (let d = 0; d < disponibilidades.length; d++) {
-      if (disponibilidades[d].dia == this.new_disponibilidad.dia) {
-        if (disponibilidades[d].hora_inicio < this.new_disponibilidad.hora_inicio && this.new_disponibilidad.hora_inicio < disponibilidades[d].hora_fin) {
-          overlap = true;
-          break;
-        }
-        if (disponibilidades[d].hora_inicio < this.new_disponibilidad.hora_fin && this.new_disponibilidad.hora_fin < disponibilidades[d].hora_fin) {
-          overlap = true;
-          break;
-        }
-        if (this.new_disponibilidad.hora_inicio < disponibilidades[d].hora_inicio && disponibilidades[d].hora_inicio < this.new_disponibilidad.hora_fin) {
-          overlap = true;
-          break;
-        }
-        if (this.new_disponibilidad.hora_inicio < disponibilidades[d].hora_fin && disponibilidades[d].hora_fin < this.new_disponibilidad.hora_fin) {
-          overlap = true;
-          break;
-        }
-      }
-    }
-
-    if (overlap) {
-      this.messageService.add({ severity: 'error', life: 10000, summary: 'Error', detail: 'Los horarios elegidos se superponen en ese día' });
-      return;
-    }
-
-    this.getControl('disponibilidades')?.setValue([...this.getControlValue('disponibilidades'), this.new_disponibilidad]);
-
-    this.new_disponibilidad = {
-      dia: -1,
-      hora_fin: 0,
-      hora_inicio: 0,
-      especialidad: '',
-    }
-  }
-
-  BorrarDisponibilidad(disponibilidad: DisponibilidadEspecialidad) {
-    let disponibilidades: DisponibilidadEspecialidad[] = this.getControlValue('disponibilidades');
-    let index = disponibilidades.indexOf(disponibilidad);
-    disponibilidades.splice(index, 1);
-    this.getControl('disponibilidades')?.setValue(disponibilidades);
-  }
-
-  async SetMaxMinHoraFinDisponibilidadDia() {
-    if (this.new_disponibilidad.dia) {
-      await this.servDisponibilidad.DisponibilidadTotalClinicaPorDia(this.new_disponibilidad.dia).then(
-        (disponibilidad: Disponibilidad) => {
-          console.log(disponibilidad);
-          this.min_hora_inicio = disponibilidad.hora_inicio;
-          this.max_hora_fin = disponibilidad.hora_fin;
-        }
-      ).catch(
-        (error) => {
-          console.log(error);
-        }
-      );
-    } else {
-      this.min_hora_inicio = 0;
-      this.max_hora_fin = 23;
-    }
-
-    this.new_disponibilidad.hora_inicio = this.min_hora_inicio;
-    this.new_disponibilidad.hora_fin = this.max_hora_fin;
   }
 }
