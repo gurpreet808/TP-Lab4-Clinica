aa159e245ea457f6f93d34a81e56c1094d573e95
gurpreet <gurpreets808@gmail.com>
2024-06-24 16:04:55 -0300

    Mejora Error 404 con estilos

--- src/app/pages/error404/error404.component.ts
+++ src/app/pages/error404/error404.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-error404',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    RouterModule
+  ],
   templateUrl: './error404.component.html',
   styleUrl: './error404.component.scss'
 })
