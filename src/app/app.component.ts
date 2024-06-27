5a01d5ac54fe107f908fbc1865caeead7f555d4d
gurpreet808 <gurpreets808@gmail.com>
2024-06-26 21:19:23 -0300

    Traduccion PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 import { AuthService } from './modulos/auth/servicios/auth.service';
 import { SpinnerService } from './modulos/spinner/servicios/spinner.service';
+import { PrimeNGConfig, Translation } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -23,11 +26,22 @@ import { SpinnerService } from './modulos/spinner/servicios/spinner.service';
 export class AppComponent implements OnInit {
   title = 'TPLab4Clinica';
 
-  constructor(public servAuth: AuthService, public servSpinner: SpinnerService) {
+  constructor(public servAuth: AuthService, public servSpinner: SpinnerService, private primengConfig: PrimeNGConfig, private http: HttpClient) {
     this.servSpinner.showWithMessage("app-init", "Cargando...");
   }
 
   ngOnInit(): void {
+    firstValueFrom(this.http.get<{ [key: string]: Translation }>('assets/archivos/es-AR.json')).then(
+      (translation: { [key: string]: Translation }) => {
+        console.log(translation["es-AR"]);
+        this.primengConfig.setTranslation(translation["es-AR"]);
+      }
+    ).catch(
+      (error: any) => {
+        console.error(error);
+      }
+    );
+
     this.servAuth.IsLoggedIn().then(
       (logueado: boolean) => {
         this.servSpinner.hideWithMessage("app-init");
