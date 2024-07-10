b48cee1104d65acc3ea34f901775bbe8ec5ffcaf
gurpreet <gurpreets808@gmail.com>
2024-07-10 15:40:38 -0300

    Agregados Mis Pacientes

--- src/app/pages/pacientes/pacientes.component.ts
+++ src/app/pages/pacientes/pacientes.component.ts
@@ -6,17 +6,28 @@ import { Subscription } from 'rxjs';
 import { HistoriaClinicaComponent } from '../../componentes/historia-clinica/historia-clinica.component';
 import { DialogModule } from 'primeng/dialog';
 import { NombreApellidoUsuarioPipe } from '../../pipes/nombre-apellido-usuario.pipe';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { UsuarioService } from '../../modulos/auth/servicios/usuario.service';
+import { Paciente, Usuario } from '../../modulos/auth/clases/usuario';
+import { TurnoItemComponent } from '../../componentes/turno-item/turno-item.component';
+import { ResaltarEstadoTurnoDirective } from '../../directivas/resaltar-estado-turno.directive';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pacientes',
   standalone: true,
   imports: [
+    FormsModule,
     HistoriaClinicaComponent,
+    TurnoItemComponent,
     DialogModule,
     ButtonModule,
+    RatingModule,
     NombreApellidoUsuarioPipe,
+    ResaltarEstadoTurnoDirective,
+    JsonPipe,
     AsyncPipe
   ],
   templateUrl: './pacientes.component.html',
@@ -24,21 +35,38 @@ import { ButtonModule } from 'primeng/button';
 })
 export class PacientesComponent implements OnInit, OnDestroy {
 
-  ids_pacientes: string[] = [];
+  pacientes: Paciente[] = [];
   turnos_suscription: Subscription;
 
-  paciente_seleccionado: string = '';
-  showHistoriaClinica: boolean = false;
+  turnos_paciente: Turno[] = [];
+  turnos_paciente_suscription: Subscription | undefined;
 
-  constructor(public servTurnos: TurnoService, public servAuth: AuthService) {
+  paciente_seleccionado: Paciente | undefined;
+  turno_seleccionado: Turno | undefined;
+  pregunta_1: string = "¿Se le hizo fácil solicitar el turno?";
+  pregunta_2: string = "¿Esperó mucho tiempo antes de ser atendido?";
+  pregunta_3: string = "¿Nuestras instalaciones estaban limpias?";
+
+  showModal: boolean = false;
+  modalAction: string = "";
+  modalTitle: string = "";
+
+  constructor(public servTurnos: TurnoService, public servAuth: AuthService, public servUsuario: UsuarioService) {
     this.turnos_suscription = this.servTurnos.TraerTurnosPorEspecialistaPorEstado(this.servAuth.usuarioActual.value!.uid, 5).subscribe(
       (turnos: Turno[]) => {
-        //check if id_paciente is already in the array, if not, add it
         turnos.forEach(
           (turno: Turno) => {
-            if (!this.ids_pacientes.includes(turno.id_paciente)) {
-              this.ids_pacientes.push(turno.id_paciente);
-            }
+            this.servUsuario.BuscarUsuarioPorUID(turno.id_paciente).then(
+              (paciente: Usuario) => {
+                if (!this.pacientes.some(p => p.uid === paciente.uid)) {
+                  this.pacientes.push(paciente as Paciente);
+                }
+              }
+            ).catch(
+              (error: any) => {
+                console.error("BuscarUsuarioPorUID", error);
+              }
+            );
           }
         );
       }
@@ -50,15 +78,42 @@ export class PacientesComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.turnos_suscription.unsubscribe();
+
+    if (this.turnos_paciente_suscription) {
+      this.turnos_paciente_suscription.unsubscribe();
+    }
   }
 
-  OcultarHistoriaClinica() {
-    this.showHistoriaClinica = false;
-    this.paciente_seleccionado = '';
+  SeleccionarPaciente(paciente: Paciente) {
+    this.paciente_seleccionado = paciente;
+
+    this.turnos_paciente_suscription = this.servTurnos.TraerTurnosPorPacientePorEstado(paciente.uid, 5).subscribe(
+      (turnos: Turno[]) => {
+        this.turnos_paciente = turnos.filter(turno => turno.id_especialista == this.servAuth.usuarioActual.value!.uid);
+      }
+    );
   }
 
-  MostrarHistoriaClinica(id_paciente: string) {
-    this.paciente_seleccionado = id_paciente;
-    this.showHistoriaClinica = true;
+  CancelarSeleccionPaciente() {
+    this.paciente_seleccionado = undefined;
+    this.turnos_paciente_suscription = undefined;
+  }
+
+  OcultarModal() {
+    this.showModal = false;
+    this.modalAction = "";
+    this.modalTitle = "";
+  }
+
+  MostrarHistoriaClinica() {
+    this.showModal = true;
+    this.modalAction = "historia_clinica";
+  }
+
+  VerResenia(_turno: Turno) {
+    this.turno_seleccionado = _turno;
+    this.showModal = true;
+    this.modalAction = "reseña";
+    this.modalTitle = "Reseña del turno";
   }
 }
